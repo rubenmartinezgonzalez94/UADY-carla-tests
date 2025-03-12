33b5699b18533bde7a65616efb44926335733115
Arturo Espinosa Romero <arturoemx@gmail.com>
2025-03-12 12:34:30 -0600

    A new, simpler, version of VPS.m was implemented.

--- simulations/Lane_detection/VPS.m
+++ simulations/Lane_detection/VPS.m
@@ -1,17 +1,10 @@
-function [Vps] = VPS(A, B, Z, h, f, Cx, Cy, theta)
+function [Vps] = VPS(f, Cx, Cy, theta)
 
-den1 = (-A*B^2*f^2*h*cos(theta)*sin(theta)^2-A*B^2*f^2*h*cos(theta)^3);
+vp1x = f * tan(theta) + Cx;
+vp1y = Cy;
 
-vp1x = (-(A*B^2*f^3*h*sin(theta)^3)-(A*B^2*Cx*f^2*h*cos(theta)*sin(theta)^2)-(A*B^2*f^3*h*cos(theta)^2*sin(theta))-(A*B^2*Cx*f^2*h*cos(theta)^3))/den1;
+vp2x = Cx - f / tan(theta);
+vp2y = Cy;
 
-vp1y = (-(A*B^2*Cy*f^2*h*cos(theta)*sin(theta)^2)-(A*B^2*Cy*f^2*h*cos(theta)^3))/den1;
-
-
-den2 = (-A^2*B*f^2*h*sin(theta)^3-A^2*B*f^2*h*cos(theta)^2*sin(theta));
-
- 
-vp2x = (-(A^2*B*Cx*f^2*h*sin(theta)^3)+(A^2*B*f^3*h*cos(theta)*sin(theta)^2)-(A^2*B*Cx*f^2*h*cos(theta)^2*sin(theta))+(A^2*B*f^3*h*cos(theta)^3))/den2;
-vp2y = (-(A^2*B*Cy*f^2*h*sin(theta)^3)-(A^2*B*Cy*f^2*h*cos(theta)^2*sin(theta)))/den2;
-
-Vps = [vp1x,vp1y;vp2x, vp2y];
+Vps = [vp1x, vp1y; vp2x, vp2y];
 end
